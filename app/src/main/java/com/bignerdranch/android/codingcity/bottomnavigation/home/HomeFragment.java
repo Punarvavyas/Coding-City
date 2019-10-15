1fbb20804705a8435ed52479902951852d9e448f
Ruize <rz376775@dal.ca>
2019-10-15 17:54:39 -0300

    Course Layout initial design

--- app/src/main/java/com/bignerdranch/android/codingcity/bottomnavigation/home/HomeFragment.java
+++ app/src/main/java/com/bignerdranch/android/codingcity/bottomnavigation/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -16,20 +18,30 @@ import com.bignerdranch.android.codingcity.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+//    private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+//        homeViewModel =
+//                ViewModelProviders.of(this).get(HomeViewModel.class);
+//        View rootView = inflater.inflate(R.layout.fragment_home, container,
+//                false);
+//        final TextView textView = rootView.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container,
+                false);
+        String[] values = new String[] { "Course1", "Course2", "Course3" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+        ListView list = rootView.findViewById(R.id.list_course);
+        list.setAdapter(adapter);
+        return rootView;
     }
 }
