c007f872c93ed17fbbd2ac8a2ca7aad092acb9ef
Ruize <rz376775@dal.ca>
2019-10-20 17:05:54 -0300

    initial lesson layout design

--- app/src/main/java/com/bignerdranch/android/codingcity/bottomnavigation/home/HomeFragment.java
+++ app/src/main/java/com/bignerdranch/android/codingcity/bottomnavigation/home/HomeFragment.java
@@ -5,19 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.bignerdranch.android.codingcity.R;
 import com.bignerdranch.android.codingcity.courseinfo.CourseContent;
@@ -25,33 +18,14 @@ import com.bignerdranch.android.codingcity.courseinfo.CourseContent;
 /**
  * This is the home page which show what course we have right now
  * @author Ruize Nie
- *
  */
 public class HomeFragment extends Fragment {
-
-//    private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-//        homeViewModel =
-//                ViewModelProviders.of(this).get(HomeViewModel.class);
-//        View rootView = inflater.inflate(R.layout.fragment_home, container,
-//                false);
-//        final TextView textView = rootView.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-
         View rootView = inflater.inflate(R.layout.fragment_home, container,
                 false);
-//        String[] values = new String[] { "Course1",  "Course2", "Course3"};
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-//                R.layout.list_item, values);
-//        ListView list = rootView.findViewById(R.id.list_course);
         ListView lv = rootView.findViewById(R.id.list_course);
         lv.setAdapter(new MyListAdpter());
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -59,11 +33,9 @@ public class HomeFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getContext(), CourseContent.class);
                 startActivity(intent);
-                //startActivityForResult(intent, 1); get the result
             }
         });
 
-//        list.setAdapter(adapter);
         return rootView;
     }
 
@@ -94,12 +66,8 @@ public class HomeFragment extends Fragment {
 
             if(convertView == null){
 
-                v = View.inflate(getContext(), R.layout.list_item, null);
+                v = View.inflate(getContext(), R.layout.list_course_item, null);
 
-                //v = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item, null);
-
-                //LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-                //inflater.inflate(R.layout.item, null);
             }else{
                 v = convertView;
             }
--- app/src/main/java/com/bignerdranch/android/codingcity/courseinfo/CourseContent.java
+++ app/src/main/java/com/bignerdranch/android/codingcity/courseinfo/CourseContent.java
@@ -1,15 +1,63 @@
 package com.bignerdranch.android.codingcity.courseinfo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.bignerdranch.android.codingcity.R;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+/**
+ * This is the Lesson page which show what lesson we have each course right now
+ * @author Ruize Nie
+ */
 public class CourseContent extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.course_content);
+
+        ListView lv = findViewById(R.id.list_lesson);
+        lv.setAdapter(new MyListAdpter());
+
+    }
+
+    private class MyListAdpter extends BaseAdapter {
+
+        @Override
+        //How many items are in the data set represented by this Adapter.
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        //Get the data item associated with the specified position in the data set.
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        //Get the row id associated with the specified position in the list.
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        //Get a View that displays the data at the specified position in the data set.
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v;
+
+            if(convertView == null){
+                //第一种写法
+                v = View.inflate(getApplicationContext(), R.layout.lsit_lesson_item, null);
+
+            }else{
+                v = convertView;
+            }
+
+            return v;
+        }
     }
 }
