9115135dbcb19a8527e1466456cb8bfaa4ba985f
Ruize <rz376775@dal.ca>
2019-11-25 15:33:42 -0400

    Tutorial done by Ruize

---
+++ app/src/main/java/com/bignerdranch/android/codingcity/tutorial/TutorialItem.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.codingcity.tutorial;
+
+/**
+ * This is the Tutorial Item using on the screen
+ * @author Ruize Nie
+ */
+public class TutorialItem {
+    String Title, Description;
+    int TutorialImg;
+
+    public TutorialItem(String title, String description, int tutorialImg) {
+        Title = title;
+        Description = description;
+        TutorialImg = tutorialImg;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public int getTutorialImg() {
+        return TutorialImg;
+    }
+
+    public void setTutorialImg(int tutorialImg) {
+        TutorialImg = tutorialImg;
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/codingcity/tutorial/TutorialViewPagerAdapter.java
@@ -0,0 +1,64 @@
+package com.bignerdranch.android.codingcity.tutorial;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bignerdranch.android.codingcity.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+/**
+ * This is the class to inflate the layout on view page
+ * @author Ruize Nie
+ */
+public class TutorialViewPagerAdapter extends PagerAdapter {
+
+    Context mContext ;
+    List<TutorialItem> mListItem;
+
+    public TutorialViewPagerAdapter(Context mContext, List<TutorialItem> mList) {
+        this.mContext = mContext;
+        this.mListItem = mList;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View tutorial = inflater.inflate(R.layout.layout_tutorial,null);
+
+        ImageView imgSlide = tutorial.findViewById(R.id.img_intro);
+        TextView title = tutorial.findViewById(R.id.title_intro);
+        TextView description = tutorial.findViewById(R.id.desc_intro);
+
+        title.setText(mListItem.get(position).getTitle());
+        description.setText(mListItem.get(position).getDescription());
+        imgSlide.setImageResource(mListItem.get(position).getTutorialImg());
+
+        container.addView(tutorial);
+        return tutorial;
+    }
+
+    @Override
+    public int getCount() {
+        return mListItem.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
